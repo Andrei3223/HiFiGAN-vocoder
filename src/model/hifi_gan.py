ad269c7c45164b1dd0cee6d100852894a4a48bbd
Andrei3223 <a.gruzitski@gmail.com>
2024-12-07 14:46:12 +0300

    model chenge

--- src/model/hifi_gan.py
+++ src/model/hifi_gan.py
@@ -2,9 +2,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import typing as tp
-from src.model.modules.generator import Generator
+# from src.model.modules.generator import Generator
 from src.model.modules.mpd import MPD
 from src.model.modules.msd import MSD
+
+from src.model.delete import Generator
 
 
 class HIFIGan(nn.Module):
